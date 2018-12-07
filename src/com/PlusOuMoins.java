a716adfd3757f93dcdd22e0f3ed4169e4d8c915c
RicoBSJ <aubrun.eric@free.fr>
2018-12-07 22:29:18 +0100

    Second commit update class PlusOuMoins

--- src/com/PlusOuMoins.java
+++ src/com/PlusOuMoins.java
@@ -1,23 +1,40 @@
 package com;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class PlusOuMoins extends Game {
 
 	public void jouer() {
 		
-		int tab[] = new int[4];
-		int essais = 0;
-		Random rand = new Random();
-		int tab[] = rand.nextInt(9);
+		int tableauCaractere[] = new int[4];
+		int i = 0;
+		int reponse = ' ',carac = ' ';
+		Scanner sc = new Scanner(System.in);
+		
+		    i = 0;
+		    System.out.println("Rentrez une lettre en minuscule, SVP ");
+		                
+		    carac = sc.nextLine().charAt(0);
+		    //Boucle de recherche dans le tableau
+		    for (i < tableauCaractere.length && carac != tableauCaractere[i])
+		      i++;
+		         
+		    //Si i < 7 c'est que la boucle n'a pas dépassé le nombre de cases du tableau 
+		    if (i < tableauCaractere.length)
+		      System.out.println(" La lettre " +carac+ " se trouve bien dans le tableau !");
+		    else //Sinon
+		      System.out.println(" La lettre " +carac+ " ne se trouve pas dans le tableau !");
+		         
+		  }while(i >= tableauCaractere.length);
 
-		System.out.println("Je joue à Plus ou Moins");
-		System.out.println("Trouvez le nombre mystere entre 0 et 100 !");
-
-		for () {
-			
-		}
+		  //Tant que la lettre de l'utilisateur ne correspond pas à une lettre du tableau    
+		  do{
+		    System.out.println("Voulez-vous essayer à nouveau ? (O/N)");
+		    reponse = sc.nextLine().charAt(0);
+		  }while(reponse != 'N' && reponse != 'O');      
+		}while (reponse == 'O');
+		                
+		System.out.println("Au revoir !");
 		
 		/*
 		do {
