8d5a4f7ab47ee6de6f6ea8c8fe5f87154ee763cb
RicoBSJ <aubrun.eric@free.fr>
2018-12-09 16:43:48 +0100

    Création d'un tableau mais recherche d'une génération aléatoire d'int

--- src/com/PlusOuMoins.java
+++ src/com/PlusOuMoins.java
@@ -1,31 +1,32 @@
 package com;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class PlusOuMoins extends Game {
 
 	public void jouer() {
-		
-		int tab[] = new int[4];
-		
-for (int i = 0; i < tab.length; i++) {
-	System.out.println("A l'emplacement " + i + " du tableau, nous avons = " + tab[i]);
-}
+
+		int[] myTab = new int[4];
+		int Nombre, essais = 0;
+		Random rand = new Random();
+		int NombreMystere = rand.nextInt(101);
+
+		System.out.println("Je joue à Plus ou Moins");
+		System.out.println("Trouvez le nombre mystere entre 0 et 100 !");
+
+		Scanner nombre = new Scanner(System.in);
+		System.out.println("Veuillez entrer un nombre...");
+		Nombre = nombre.nextInt();
+
+		if (Nombre < NombreMystere || Nombre != NombreMystere) {
+			System.out.println("C'est plus ! ");
+			essais++;
+		} else if (Nombre > NombreMystere) {
+			System.out.println("C'est moins !");
+			essais++;
+		} else {
+			System.out.println("Trouver en " + essais + " essais ! Bien Joué !");
+		}
 	}
 }
-		
-		/*
-		do {
-			Scanner nombre = new Scanner(System.in);
-			System.out.println("Veuillez entrer un nombre...");
-			Nombre = nombre.nextInt();
-
-			if (Nombre < NombreMystere) {
-				System.out.println("C'est plus ! ");
-				essais++;
-			} else if (Nombre > NombreMystere) {
-				System.out.println("C'est moins !");
-				essais++;
-			} else {
-				System.out.println("Trouver en " + essais + " essais ! Bien Joué !");
-			}
-		} while (Nombre != NombreMystere);
-		*/
