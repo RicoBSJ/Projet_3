07c60641b3f7fc70c44971e462eb5648a88bae6d
RicoBSJ <aubrun.eric@free.fr>
2018-12-09 17:16:28 +0100

    Utilisation de la fonction boolean

--- src/com/PlusOuMoins.java
+++ src/com/PlusOuMoins.java
@@ -1,32 +1,37 @@
 package com;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class PlusOuMoins extends Game {
 
 	public void jouer() {
 
-		int[] myTab = new int[4];
-		int Nombre, essais = 0;
-		Random rand = new Random();
-		int NombreMystere = rand.nextInt(101);
-
-		System.out.println("Je joue à Plus ou Moins");
-		System.out.println("Trouvez le nombre mystere entre 0 et 100 !");
-
-		Scanner nombre = new Scanner(System.in);
-		System.out.println("Veuillez entrer un nombre...");
-		Nombre = nombre.nextInt();
-
-		if (Nombre < NombreMystere || Nombre != NombreMystere) {
-			System.out.println("C'est plus ! ");
-			essais++;
-		} else if (Nombre > NombreMystere) {
-			System.out.println("C'est moins !");
-			essais++;
-		} else {
-			System.out.println("Trouver en " + essais + " essais ! Bien Joué !");
-		}
+	    int[] random = new int[4];
+	    int i = 0;
+	    boolean equals;
+	    @SuppressWarnings("unused")
+		Random rand = new Random(); {
+	         
+	    	//write\\
+	    	while(i != 4) {
+	            random[i] = (int)(Math.random()*4);
+	            equals = false;
+	            for(int j = 0; j != i; j++) {
+	                if(random[j] == random[i]) {
+	                    equals = true;
+	                }
+	            }
+	            if(!equals) {
+	                i++;
+	            }
+	        }
+	        //write\\
+	 
+	        //read\\
+	        for(int k = 0; k != 4; k++) {
+	            System.out.print(random[k]);
+	        }
+	        //read\\
+	    }
 	}
 }
