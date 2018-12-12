1f2e93abd36202715e847872b3ac18eab6cf261a
RicoBSJ <aubrun.eric@free.fr>
2018-12-12 20:29:50 +0100

    Recherche tableau

--- src/com/PlusOuMoins.java
+++ src/com/PlusOuMoins.java
@@ -1,37 +1,43 @@
 package com;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class PlusOuMoins extends Game {
 
 	public void jouer() {
+		int tableauJeu[] = {0, 0, 0, 0};
+		int i = 0;
+		char reponse = ' ';
+		int nbr = ' ';
+		Scanner sc = new Scanner(System.in);
 
-	    int[] random = new int[4];
-	    int i = 0;
-	    boolean equals;
-	    @SuppressWarnings("unused")
-		Random rand = new Random(); {
-	         
-	    	//write\\
-	    	while(i != 4) {
-	            random[i] = (int)(Math.random()*4);
-	            equals = false;
-	            for(int j = 0; j != i; j++) {
-	                if(random[j] == random[i]) {
-	                    equals = true;
-	                }
-	            }
-	            if(!equals) {
-	                i++;
-	            }
-	        }
-	        //write\\
-	 
-	        //read\\
-	        for(int k = 0; k != 4; k++) {
-	            System.out.print(random[k]);
-	        }
-	        //read\\
-	    }
+		do {// Boucle principale
+			do {// On répète cette boucle tant que l'utilisateur n'a pas rentré une lettre
+				// figurant dans le tableau
+				i = 0;
+				System.out.println("Rentrez un chiffre, SVP ");
+				
+				nbr = sc.nextInt();
+				sc.nextLine();
+				// Boucle de recherche dans le tableau
+				while (i < tableauJeu.length && nbr != tableauJeu[i])
+					i++;
+
+				// Si i < 7 c'est que la boucle n'a pas dépassé le nombre de cases du tableau
+				if (i < tableauJeu.length)
+					System.out.println("Le chiffre " + nbr + " se trouve bien dans le tableau !");
+				else // Sinon
+					System.out.println("Le chiffre " + nbr + " ne se trouve pas dans le tableau !");
+
+			} while (i >= tableauJeu.length);
+
+			// Tant que la lettre de l'utilisateur ne correspond pas à une lettre du tableau
+			do {
+				System.out.println("Voulez-vous essayer à nouveau ? (O/N)");
+				reponse = sc.nextLine().charAt(0);
+			} while (reponse != 'N' && reponse != 'O');
+		} while (reponse == 'O');
+
+		System.out.println("Au revoir !");
 	}
 }
