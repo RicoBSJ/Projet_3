23227e0b714f4d3c85adcba26378615c7537e240
aubrune <aubrune@P520.ari.asso.fr>
2018-12-14 12:35:15 +0100

    Second commit Job POM

--- src/com/PlusOuMoins.java
+++ src/com/PlusOuMoins.java
@@ -5,19 +5,17 @@ import java.util.Scanner;
 public class PlusOuMoins extends Game {
 
 	public void jouer() {
-		int nbr1 = '0';
+		int nbr1 = 0, nbr2 = 0, nbr3 = 0, nbr4 = 0;
+		int tableauJeu[] = { nbr1, nbr2, nbr3, nbr4 };
 		String j1 = new String();
 		j1 = String.valueOf(nbr1);
-		int nbr2 = '0';
 		String j2 = new String();
 		j2 = String.valueOf(nbr2);
-		int nbr3 = ' ';
 		String j3 = new String();
 		j3 = String.valueOf(nbr3);
-		int nbr4 = ' ';
 		String j4 = new String();
 		j4 = String.valueOf(nbr4);
-		String tableauJeu[] = { "j1", "j2", "j3", "j4" };
+//		String tableauJeu[] = { "j1", "j2", "j3", "j4" };
 		int i = 0;
 		char reponse = ' ';
 		Scanner sc = new Scanner(System.in);
@@ -46,14 +44,14 @@ public class PlusOuMoins extends Game {
 				// Si i < 7 c'est que la boucle n'a pas dépassé le nombre de cases du tableau
 				if (i < tableauJeu.length) {
 					System.out.println("Le chiffre 1 se trouve bien dans le tableau !");
-					System.out.println("Le chiffre " + j2 + " se trouve bien dans le tableau !");
-					System.out.println("Le chiffre " + j3 + " se trouve bien dans le tableau !");
-					System.out.println("Le chiffre " + j4 + " se trouve bien dans le tableau !");
+					System.out.println("Le chiffre 2 se trouve bien dans le tableau !");
+					System.out.println("Le chiffre 3 se trouve bien dans le tableau !");
+					System.out.println("Le chiffre 4 se trouve bien dans le tableau !");
 				} else { // Sinon
-					System.out.println("Le chiffre " + j1 + " ne se trouve pas dans le tableau !");
-					System.out.println("Le chiffre " + j2 + " ne se trouve pas dans le tableau !");
-					System.out.println("Le chiffre " + j3 + " ne se trouve pas dans le tableau !");
-					System.out.println("Le chiffre " + j4 + " ne se trouve pas dans le tableau !");
+					System.out.println("Le chiffre 1 ne se trouve pas dans le tableau !");
+					System.out.println("Le chiffre 2 ne se trouve pas dans le tableau !");
+					System.out.println("Le chiffre 3 ne se trouve pas dans le tableau !");
+					System.out.println("Le chiffre 4 ne se trouve pas dans le tableau !");
 				}
 			} while (i >= tableauJeu.length);
 
