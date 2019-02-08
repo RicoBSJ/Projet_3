3cba0c78f809ee46825ae7f064651c231f47dcd1
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2019-02-08 22:46:02 +0100

    1 Commit Home 08_02

--- src/com/PlusOuMoinsDefenseur.java
+++ src/com/PlusOuMoinsDefenseur.java
@@ -1,64 +1,16 @@
 package com;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class PlusOuMoinsDefenseur extends Game {
-
-	private int[] createTab() {
-		int[] resultat = { 0, 0, 0, 0 };
-		System.out.println("Veuillez taper 4 chiffres :");
-		Scanner sc = new Scanner(System.in);
-		String result = sc.nextLine();
-		for (int i = 0; i < resultat.length; i++) {
-			String carac = Character.toString(result.charAt(i));
-			resultat[i] = Integer.parseInt(carac);
-		}
-		return resultat;
-	}
-
-	private int[] askTab() {
-		int[] resultat = { 0, 0, 0, 0 };
-		Random rand = new Random();
-		for (int i = 0; i < resultat.length; i++) {
-			resultat[i] = rand.nextInt(9);
-		}
-		return resultat;
-	}
-
-	private void dev(int[] tab) {
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i]);
-		}
-		System.out.println();
-	}
-
-	private String compare(int tableauDefenseur[], int tableauOrdinateur[]) {
-		String resultat = "";
-		for (int i = 0; i < tableauOrdinateur.length; i++) {
-
-			if (tableauOrdinateur[i] == tableauDefenseur[i]) {
-				resultat += "=";
-			} else if (tableauOrdinateur[i] < tableauDefenseur[i]) {
-				resultat += "-";
-			} else if (tableauOrdinateur[i] > tableauDefenseur[i]) {
-				resultat += "+";
-			} else {
-				break;
-			}
-		}
-		return resultat;
-	}
 
 	public void jouer() {
 		// A remplacer ensuite par une liste
-		int[] tableauDefenseur = createTab();
+		int[] tableauDefenseur = askTab();
 		int[] tableauOrdinateur;
 		// dev(tableauDefenseur);
 		boolean win = false;
 
 		while (!win) {
-			tableauOrdinateur = askTab();
+			tableauOrdinateur = createTabComputer();
 			dev(tableauOrdinateur);
 			String compareResult = compare(tableauOrdinateur, tableauDefenseur);
 			System.out.println(compareResult);
@@ -66,3 +18,7 @@ public class PlusOuMoinsDefenseur extends Game {
 		}
 	}
 }
+/*
+ * 1234 ++=+ 2335 ++=+ 3436 ++=+ 4537 =+=+ 4638 =+== 4738 =+== 4838 =+== 4938
+ * ====
+ */
