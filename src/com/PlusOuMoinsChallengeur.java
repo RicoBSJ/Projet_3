3ee2d2230b252e26e5bab9311911e1ffabdebe86
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2019-02-08 22:44:51 +0100

    1 Commit Home 08_02

--- src/com/PlusOuMoinsChallengeur.java
+++ src/com/PlusOuMoinsChallengeur.java
@@ -1,55 +1,7 @@
 package com;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class PlusOuMoinsChallengeur extends Game {
 	
-	private int[] createTab() {
-		int[] resultat = { 0, 0, 0, 0 };
-		Random rand = new Random();
-		for (int i = 0; i < resultat.length; i++) {
-			resultat[i] = rand.nextInt(9);
-		}
-		return resultat;
-	}
-
-	private int[] askTab() {
-		int[] resultat = { 0, 0, 0, 0 };
-		System.out.println("Veuillez taper 4 chiffres :");
-		Scanner sc = new Scanner(System.in);
-		String result = sc.nextLine();
-		for (int i = 0; i < resultat.length; i++) {
-			String carac = Character.toString(result.charAt(i));
-			resultat[i] = Integer.parseInt(carac);
-		}
-		return resultat;
-	}
-
-	private void dev(int[] tab) {
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i]);
-		}
-		System.out.println();
-	}
-
-	public String compare(int tableauJeu[], int tableauJoueur[]) {
-		String resultat = "";
-		for (int i = 0; i < tableauJoueur.length; i++) {
-
-			if (tableauJoueur[i] == tableauJeu[i]) {
-				resultat += "=";
-			} else if (tableauJoueur[i] < tableauJeu[i]) {
-				resultat += "-";
-			} else if (tableauJoueur[i] > tableauJeu[i]) {
-				resultat += "+";
-			} else {
-				break;
-			}
-		}
-		return resultat;
-	}
-
 	public void jouer() {
 		// A remplacer ensuite par une liste
 		int[] tableauJeu = createTab();
