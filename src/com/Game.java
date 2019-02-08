560930066d800dbce8e55265a926b8753f26565f
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2019-02-08 22:42:38 +0100

    1 Commit Home 08_02

--- src/com/Game.java
+++ src/com/Game.java
@@ -1,6 +1,68 @@
 package com;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public abstract class Game {
+
+	protected int[] createTab() {
+		int[] resultat = { 0, 0, 0, 0 };
+		Random rand = new Random();
+		for (int i = 0; i < resultat.length; i++) {
+			resultat[i] = rand.nextInt(9);
+		}
+		return resultat;
+	}
+
+	protected int[] createTabComputer() {
+		/*
+		 * premier essai aléatoire réponse donnée par le défenseur comparaison entre
+		 * l'essai et la réponse donnée si + rajouter le double si - diviser par deux si
+		 * = garder return réponse
+		 */
+		int[] resultat = { 0, 0, 0, 0 };
+		Random rand = new Random();
+		for (int i = 0; i < resultat.length; i++) {
+			resultat[i] = rand.nextInt(9);
+		}
+		return resultat;
+	}
+
+	protected int[] askTab() {
+		int[] resultat = { 0, 0, 0, 0 };
+		System.out.println("Veuillez taper 4 chiffres :");
+		Scanner sc = new Scanner(System.in);
+		String result = sc.nextLine();
+		for (int i = 0; i < resultat.length; i++) {
+			String carac = Character.toString(result.charAt(i));
+			resultat[i] = Integer.parseInt(carac);
+		}
+		return resultat;
+	}
+
+	protected void dev(int[] tab) {
+		for (int i = 0; i < tab.length; i++) {
+			System.out.print(tab[i]);
+		}
+		System.out.println();
+	}
+
+	protected String compare(int tableauDefenseur[], int tableauOrdinateur[]) {
+		String resultat = "";
+		for (int i = 0; i < tableauOrdinateur.length; i++) {
+
+			if (tableauOrdinateur[i] == tableauDefenseur[i]) {
+				resultat += "=";
+			} else if (tableauOrdinateur[i] < tableauDefenseur[i]) {
+				resultat += "-";
+			} else if (tableauOrdinateur[i] > tableauDefenseur[i]) {
+				resultat += "+";
+			} else {
+				break;
+			}
+		}
+		return resultat;
+	}
 
 	public void jouer() {
 
