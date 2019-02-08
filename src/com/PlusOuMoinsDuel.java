ce70f5619061f28111fdf5a2a4a04714760f07ac
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2019-02-08 22:47:55 +0100

    2 Commit Home 08_02

--- src/com/PlusOuMoinsDuel.java
+++ src/com/PlusOuMoinsDuel.java
@@ -3,74 +3,7 @@ package com;
 import java.util.Random;
 import java.util.Scanner;
 
-public class PlusOuMoinsJoueur extends Game {
-
-	private int[] createTab() {
-		int[] resultat = { 0, 0, 0, 0 };
-		Random rand = new Random();
-		for (int i = 0; i < resultat.length; i++) {
-			resultat[i] = rand.nextInt(9);
-		}
-		return resultat;
-	}
-
-	private int[] askTabJoueur() {
-		int[] resultat = { 0, 0, 0, 0 };
-		System.out.println("Joueur, veuillez taper 4 chiffres :");
-		Scanner sc = new Scanner(System.in);
-		String result = sc.nextLine();
-		for (int i = 0; i < resultat.length; i++) {
-			String carac = Character.toString(result.charAt(i));
-			resultat[i] = Integer.parseInt(carac);
-		}
-		return resultat;
-	}
-
-	private int[] askTabOrdinateur() {
-		int[] resultat = { 0, 0, 0, 0 };
-		Random rand = new Random();
-		for (int i = 0; i < resultat.length; i++) {
-			resultat[i] = rand.nextInt(9);
-		}
-		return resultat;
-	}
-
-	private void dev(int[] tab) {
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i]);
-		}
-		System.out.println();
-	}
-
-	private String compare(int tableauJeu[], int tableauJoueur[], int tableauOrdinateur[]) {
-		
-		String resultat = "";
-		
-		for (int i = 0; i < tableauJoueur.length; i++) {
-
-			if (tableauJoueur[i] == tableauJeu[i]) {
-				resultat += "=";
-			} else if (tableauJoueur[i] < tableauJeu[i]) {
-				resultat += "-";
-			} else if (tableauJoueur[i] > tableauJeu[i]) {
-				resultat += "+";
-			}
-
-			for (int j = 0; j < tableauOrdinateur.length; j++) {
-
-				if (tableauJoueur[j] == tableauOrdinateur[j]) {
-					resultat += "=";
-				} else if (tableauJoueur[j] < tableauOrdinateur[j]) {
-					resultat += "-";
-				} else if (tableauJoueur[j] > tableauOrdinateur[j]) {
-					resultat += "+";
-				} else {
-					break;
-				}
-			}
-		}
-		return resultat;
-	}
+public class PlusOuMoinsDuel extends Game {
 
 	public void jouer() {
 		// A remplacer ensuite par une liste
@@ -80,14 +13,14 @@ public class PlusOuMoinsJoueur extends Game {
 		// dev(tableauJeu);
 		boolean win = false;
 
-		while (!win) {
-			tableauJoueur = askTabJoueur();
-			tableauOrdinateur = askTabOrdinateur();
-			dev(tableauJoueur);
-			dev(tableauOrdinateur);
-			String compareResult = compare(tableauJeu, tableauJoueur, tableauOrdinateur);
-			System.out.println(compareResult);
-			win = compareResult.equals("====");
-		}
+		// while (!win) {
+		// tableauJoueur = askTab();
+		// tableauOrdinateur = createTabComputer();
+		// dev(tableauJoueur);
+		// dev(tableauOrdinateur);
+		// String compareResult = compare(tableauJeu, tableauJoueur, tableauOrdinateur);
+		// System.out.println(compareResult);
+		// win = compareResult.equals("====");
+		// }
 	}
 }
