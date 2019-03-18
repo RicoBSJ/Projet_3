8adc0d421e348003a0e215056f0b2071dda5c9eb
aubrune <aubrune@P520.ari.asso.fr>
2019-03-18 16:38:59 +0100

    2 Commit Job 18_03

--- src/com/MastermindChallengeur.java
+++ src/com/MastermindChallengeur.java
@@ -4,6 +4,28 @@ public class MastermindChallengeur extends Game {
 	
 	public void jouer() {
 		// A remplacer ensuite par une liste
+		System.out.println("\rVous avez droit à " + Constante.nombreEssai + " essais");
+		int[] tableauJeu = createTab();
+		System.out.print("\rL'ordinateur a choisi : ");
+		int[] tableauJoueur;
+		dev(tableauJeu);
+		boolean win = false;
+		int nbrEssaiCh = 0;
 
+		while (!win && nbrEssaiCh < Constante.nombreEssai) {
+			nbrEssaiCh++;
+			tableauJoueur = askTab();
+			System.out.print("\rVous essayez avec : ");
+			dev(tableauJoueur);
+			String compareResult = compare(tableauJoueur, tableauJeu);
+			System.out.println(compareResult);
+			win = compareResult.equals(resultatGagnant());
+		}
+		if (win == true) {
+			System.out.println("\rVous avez trouvé la combinaison en " + nbrEssaiCh + " essai(s)");
+		} else {
+			System.out.println("\rVous avez perdu !");
+		}
+		optionsJeux();
 		}
 	}
