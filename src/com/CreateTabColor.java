85ce486f4448491c6fde60c5c8b75bb3091385b0
RicoBSJ <aubrun.eric@free.fr>
2019-03-20 19:26:27 +0100

    2 Commit Home 20_03

--- src/com/CreateTabColor.java
+++ src/com/CreateTabColor.java
@@ -4,20 +4,22 @@ public class CreateTabColor {
 
 	public static void main(String[] args) {
 
-		String[] tabColor = new String[Constante.longueurCombinaison];
-		String couleurVert = "vert";
-		String couleurOrange = " orange";
-		String couleurJaune = " jaune";
-		String couleurGris = " gris";
-		String couleurNoir = " noir";
-		tabColor[0] = couleurVert;
-		tabColor[1] = couleurOrange;
-		tabColor[2] = couleurJaune;
-		tabColor[3] = couleurGris;
-		tabColor[4] = couleurNoir;
+//		String[] tabColor = new String[Constante.longueurCombinaison];
+//		String couleurVert = "vert";
+//		String couleurOrange = " orange";
+//		String couleurJaune = " jaune";
+//		String couleurGris = " gris";
+//		String couleurNoir = " noir";
+//		tabColor[0] = couleurVert;
+//		tabColor[1] = couleurOrange;
+//		tabColor[2] = couleurJaune;
+//		tabColor[3] = couleurGris;
+//		tabColor[4] = couleurNoir;
+		
+		String[] tabColor = {Constante.couleurVert, Constante.couleurOrange, Constante.couleurJaune, Constante.couleurGris, Constante.couleurNoir};
 
 		for (int i = 0; i < tabColor.length; i++) {
-			System.out.print(tabColor[i]);
+			System.out.println(tabColor[i]);
 		}
 		System.out.println();
 	}
--- src/com/Game.java
+++ src/com/Game.java
@@ -14,26 +14,18 @@ public abstract class Game {
 	}
 
 	protected String[] createTabColor() {
-		String[] tabColor = new String[Constante.longueurCombinaison];
-		String couleurVert = "vert";
-		String couleurOrange = "orange";
-		String couleurJaune = "jaune";
-		String couleurGris = "gris";
-		String couleurNoir = "noir";
-		tabColor[0] = couleurVert;
-		tabColor[1] = couleurOrange;
-		tabColor[2] = couleurJaune;
-		tabColor[3] = couleurGris;
-		tabColor[4] = couleurNoir;
-		
-		System.out.println(tabColor);
-		
+
+		String[]tabColor = {Constante.couleurVert, Constante.couleurOrange, Constante.couleurJaune, Constante.couleurGris, Constante.couleurNoir};
+		for (int i = 0; i < tabColor.length; i++) {
+			System.out.println(tabColor[i]);
+		}
+		System.out.println();
 		return tabColor;
 	}
-
 	/*
-	 * couleurVert couleurOrange couleurJaune couleurGris couleurNoir couleurBleu
-	 * couleurRouge couleurViolet couleurBleuFonce couleurMarron
+	 * static public String couleurVert = "vert"; static public String couleurOrange
+	 * = "orange"; static public String couleurJaune = "jaune"; static public String
+	 * couleurGris = "gris"; static public String couleurNoir = "noir";
 	 */
 
 	protected void optionsJeux() {
