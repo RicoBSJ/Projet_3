76b0658ab37ec5a87d39e683496bb36bec58e22e
aubrune <aubrune@P520.ari.asso.fr>
2019-05-10 12:21:03 +0200

    2 Commit Job 10_05

--- src/com/MastermindDefenseur.java
+++ src/com/MastermindDefenseur.java
@@ -13,9 +13,9 @@ public class MastermindDefenseur extends Game {
 	public void jouer() {
 		// A remplacer ensuite par une liste
 		initializeTab();
-		System.out.println("\rL'ordinateur a droit Ã  " + Constante.nombreEssai + " essais");
+		System.out.println("\rL'ordinateur a droit à " + Constante.nombreEssai + " essais");
 		String[] tableauDefenseur = askTabColor();
-		System.out.print("\rLe dÃ©fenseur a entrÃ© la combinaison : ");
+		System.out.print("\rLe défenseur a entré la combinaison : ");
 		dev(tableauDefenseur);
 		boolean win = false;
 
@@ -34,7 +34,6 @@ public class MastermindDefenseur extends Game {
 	}
 
 	private boolean compareDefenseur(String tableauJoueur[], String tableauJeu[]) {
-		nbrEssaiDef++;
 		int nbrPresent = 0;
 		int nbrBienPlace = 0;
 		for (int i = 0; i < tableauJoueur.length; i++) {
@@ -50,44 +49,53 @@ public class MastermindDefenseur extends Game {
 				}
 			}
 		}
-		System.out.print("presenteEtMalPlace :");
-		System.out.println(Arrays.toString(presenteEtMalPlace));
-		System.out.print("presenteEtBienPlace :");
-		System.out.println(Arrays.toString(presenteEtBienPlace));
-		System.out.println(nbrPresent + " couleurs sont prÃ©sentes et " + nbrBienPlace + " sont bien placÃ©es");
+		System.out.println(nbrPresent + " couleurs sont présentes et " + nbrBienPlace + " sont bien placées");
 		return nbrBienPlace == tableauJeu.length;
 	}
 
+	@SuppressWarnings("unused")
 	private String[] creerCombinaison() {
 		if (nbrEssaiDef == 0) {
 			tabComp = createTabColor(Constante.tabColor);
 			if (nbrEssaiDef != 0) {
 				tabComp = presenteEtBienPlace;
 			}
-			for (int i = 0; i < Constante.longueurCombinaison; i++) {
-				copy = tabComp;
-				if (tabComp[i] == null && presenteEtMalPlace[i] != null) {
+		}
+		for (int i = 0; i < Constante.longueurCombinaison; i++) {
+			boolean colorInTab;
+			copy = tabComp;
+			if (tabComp[i] == null) {
+				colorInTab = presenteEtMalPlace[i] != null;
+				if (colorInTab = true) {
+					//Ligne remplissant le tableau de null
 					tabComp[i] = presenteEtMalPlace[i];
-					if (tabComp[i] == null && presenteEtMalPlace[i] == null) {
-						presenteEtMalPlace[i] = Constante.tabColor[i];
-					}
-					tabComp[i] = presenteEtMalPlace[i];
-					presenteEtMalPlace[i] = null;
+				} else {
+					tabComp[i] = Constante.tabColor[i];
 				}
 			}
+			presenteEtBienPlace[i] = tabComp[i];
+			presenteEtMalPlace[i] = null;
 		}
 		return copy;
 	}
 
 	/*
-	 * private creerCombinaison { Si nbrEssaiDef = 0 > createTabColor Si nbrEssaiDef
-	 * != 0 > rÃ©cupÃ©rer presenteEtBienPlace for () { CrÃ©er une copie de
-	 * presenteEtBienPlace Si l'index sur lequel on se trouve = null, on vÃ©rifie
-	 * qu'il y des couleurs disponibles dans presenteEtMalPlace S'il y en a on prend
-	 * une couleur qui remlacera null S'il n'y en a pas, on prend unr nouvelle
-	 * couleur alÃ©atoire dans presenteEtMalPlace On remplace null par cette couleur
-	 * dans presenteEtBienPlace et cette couleur par null dans presenteEtMalPlace
-	 * return copie }
+	 * Créer une méthode private creerCombinaison
+	 * {
+	 * Si nbrEssaiDef = 0 alors on utilise la méthode createTabColor
+	 * Si nbrEssaiDef != 0 alors on récupère presenteEtBienPlace
+	 * for ()
+	 * {
+	 * On crée une copie de presenteEtBienPlace
+	 * 
+	 * Si l'index sur lequel on se trouve = null, on vérifie s'il y a des couleurs disponibles dans presenteEtMalPlace
+	 * 		S'il y en a on prend une couleur qui remlacera null
+	 * 		S'il n'y en a pas, on prend une nouvelle couleur alétoire dans Constante.tabColor
+	 * On remplace null par cette couleur dans presenteEtBienPlace
+	 * Et cette couleur par null dans presenteEtMalPlace
+	 * 
+	 * return copie
+	 * }
 	 */
 
 	private void initializeTab() {
