fabf4a9175aebd1e7d102a4d57ad87f288d5861c
RicoBSJ <aubrun.eric@free.fr>
2019-05-29 22:34:54 +0200

    4 Commit Home 29_05

--- src/com/log4jExample.java
+++ src/com/log4jExample.java
@@ -1,18 +1,36 @@
 package com;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
+
+import org.apache.log4j.*;
 
 public class log4jExample {
-	/* Get actual class name to be printed on */
-	private static Logger log = Logger.getLogger(log4jExample.class);
 
-	public static void main(String[] args) {
+	private static Logger logger = Logger.getLogger(log4jExample.class);
 
-		log.debug("msg de debogage");
-		log.info("msg d'information");
-		log.warn("msg d'avertissement");
-		log.error("msg d'erreur");
-		log.fatal("msg d'erreur fatale");
+	public static void main(String[] args) throws IOException {
+
+		logger.trace("msg de traces d'exécution");
+		logger.debug("msg de debogage");
+		logger.info("msg d'information");
+		logger.warn("msg d'avertissement");
+		logger.error("msg d'erreur");
+		logger.fatal("msg d'erreur fatale");
+		
+		Logger.getRootLogger().addAppender(new ConsoleAppender());
+		Logger log1 = Logger.getLogger("org");
+		log1.setAdditivity(false);
+		log1.addAppender(new FileAppender(new SimpleLayout(), "progx.log"));
+		Logger log2 = Logger.getLogger("org.progx");
+
+		/*
+		 * TRACE
+		 * DEBUG
+		 * INFO
+		 * WARN
+		 * ERROR
+		 * FATAL
+		 */
 
 	}
 }
