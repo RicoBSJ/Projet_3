8729173a7747436f822df13cd28acd28ca9126bf
RicoBSJ <aubrun.eric@free.fr>
2019-06-03 23:46:04 +0200

    5 Commit Home 03_06

--- src/com/WriteFile2.java
+++ src/com/WriteFile2.java
@@ -1,6 +1,6 @@
 package com;
 
-//Packages à importer afin d'utiliser les objets
+//Packages Ã  importer afin d'utiliser les objets
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,32 +11,32 @@ public class WriteFile2 {
 	
  public static void main(String[] args) {
 	 
-    // Nous déclarons nos objets en dehors du bloc try/catch
+    // Nous dÃ©clarons nos objets en dehors du bloc try/catch
     FileInputStream fis = null;
     FileOutputStream fos = null;
 
     try {
        // On instancie nos objets :
        // fis va lire le fichier
-       // fos va écrire dans le nouveau !
+       // fos va Ã©crire dans le nouveau !
        fis = new FileInputStream(new File("test.txt"));
        fos = new FileOutputStream(new File("test2.txt"));
 
-       // On crée un tableau de byte pour indiquer le nombre de bytes lus à
+       // On crÃ©e un tableau de byte pour indiquer le nombre de bytes lus Ã 
        // chaque tour de boucle
        byte[] buf = new byte[8];
 
-       // On crée une variable de type int pour y affecter le résultat de
+       // On crÃ©e une variable de type int pour y affecter le rÃ©sultat de
        // la lecture
        // Vaut -1 quand c'est fini
        int n = 0;
 
        // Tant que l'affectation dans la variable est possible, on boucle
-       // Lorsque la lecture du fichier est terminée l'affectation n'est
+       // Lorsque la lecture du fichier est terminÃ©e l'affectation n'est
        // plus possible !
        // On sort donc de la boucle
        while ((n = fis.read(buf)) >= 0) {
-          // On écrit dans notre deuxième fichier avec l'objet adéquat
+          // On Ã©crit dans notre deuxiÃ¨me fichier avec l'objet adÃ©quat
           fos.write(buf);
           // On affiche ce qu'a lu notre boucle au format byte et au
           // format char
@@ -44,25 +44,25 @@ public class WriteFile2 {
              System.out.print("\t" + bit + "(" + (char) bit + ")");
           }
           System.out.println("");
-          //Nous réinitialisons le buffer à vide
-          //au cas où les derniers byte lus ne soient pas un multiple de 8
-          //Ceci permet d'avoir un buffer vierge à chaque lecture et ne pas avoir de doublon en fin de fichier
+          //Nous rÃ©initialisons le buffer Ã  vide
+          //au cas oÃ¹ les derniers byte lus ne soient pas un multiple de 8
+          //Ceci permet d'avoir un buffer vierge Ã  chaque lecture et ne pas avoir de doublon en fin de fichier
           buf = new byte[8];
 
        }
-       System.out.println("Copie terminée !");
+       System.out.println("Copie terminÃ©e !");
 
     } catch (FileNotFoundException e) {
-       // Cette exception est levée si l'objet FileInputStream ne trouve
+       // Cette exception est levÃ©e si l'objet FileInputStream ne trouve
        // aucun fichier
        e.printStackTrace();
     } catch (IOException e) {
-       // Celle-ci se produit lors d'une erreur d'écriture ou de lecture
+       // Celle-ci se produit lors d'une erreur d'Ã©criture ou de lecture
        e.printStackTrace();
     } finally {
-       // On ferme nos flux de données dans un bloc finally pour s'assurer
-       // que ces instructions seront exécutées dans tous les cas même si
-       // une exception est levée !
+       // On ferme nos flux de donnÃ©es dans un bloc finally pour s'assurer
+       // que ces instructions seront exÃ©cutÃ©es dans tous les cas mÃªme si
+       // une exception est levÃ©e !
        try {
           if (fis != null)
              fis.close();
