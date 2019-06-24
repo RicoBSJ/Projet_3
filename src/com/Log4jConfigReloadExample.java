107389c8a2deadb0ff88df569c25552d9d25f3da
RicoBSJ <aubrun.eric@free.fr>
2019-06-24 23:48:25 +0200

    4 Commit Home 24_06

--- src/com/Log4jConfigReloadExample.java
+++ src/com/Log4jConfigReloadExample.java
@@ -2,28 +2,25 @@ package com;
 
 import org.apache.log4j.Logger;
 
-public class Log4jConfigReloadExample
-{
-    private static final String LOG_FILE_PATH = "C:/Users/eric/git/Projet_3/log4j.xml";
-     
-    public static void main(String[] args) throws InterruptedException
-    {
-        //Configure logger service
-        Log4jConfigurator.getInstance().initilize(LOG_FILE_PATH);
-         
-        //Get logger instance
-        Logger LOGGER = Logger.getLogger(Log4jConfigReloadExample.class);
-         
-        //Print the log messages and wait for log4j changes
-        while(true)
-        {
-            //Debug level log message
-            LOGGER.debug("A debug message !!");
-            //Info level log message
-            LOGGER.info("A info message !!");
-             
-            //Wait between log messages
-            Thread.sleep(2000);
-        }
-    }
+public class Log4jConfigReloadExample {
+	private static final String LOG_FILE_PATH = "C:/Users/eric/git/Projet_3/log4j.xml";
+
+	public static void main(String[] args) throws InterruptedException {
+		// Configure logger service
+		Log4jConfigurator.getInstance().initilize(LOG_FILE_PATH);
+
+		// Get logger instance
+		Logger LOGGER = Logger.getLogger(Log4jConfigReloadExample.class);
+
+		// Print the log messages and wait for log4j changes
+		while (true) {
+			// Debug level log message
+			LOGGER.debug("A debug message !!");
+			// Info level log message
+			LOGGER.info("A info message !!");
+
+			// Wait between log messages
+			Thread.sleep(2000);
+		}
+	}
 }
